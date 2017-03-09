ba9027136933e8963e323426f3e629e6dbdf5ed5
ChanYvonne <ychan1352@gmail.com>
2017-03-09 12:47:09 -0500

    ugh why are the circles depressed

--- svg.js
+++ svg.js
@@ -7,75 +7,86 @@ var rid;
 
 //change circle color upon click
 var change = function(e) {
-	if (this.getAttribute("fill")=="red") {
-		svg.appendChild(makeCirc(Math.random()*width, Math.random()*height));
-		svg.removeChild(this);
-		return ;
-	}
-	this.setAttribute("fill", "red");
-	e.stopPropagation();
+    if (this.getAttribute("fill")=="red") {
+	svg.appendChild(makeCirc(Math.random()*width, Math.random()*height));
+	svg.removeChild(this);
+	return ;
+    }
+    this.setAttribute("fill", "red");
+    e.stopPropagation();
 };
 
 //make circle at x,y
 var makeCirc = function(x,y) {
-	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c.setAttribute("cx", x);
-	c.setAttribute("cy", y);
-	c.setAttribute("r", (Math.random()*30)+20);
-	c.setAttribute("fill", "aliceblue");
-	c.setAttribute("stroke", "black");
-	c.addEventListener('click', change);
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", (Math.random()*30)+20);
+    c.setAttribute("fill", "aliceblue");
+    c.setAttribute("stroke", "black");
+    c.setAttribute("xvol", 1);
+    c.setAttribute("yvol", 1);
+    c.addEventListener('click', change);
 
-	return c;
+    return c;
 };
 
 //add a circle to svg
 var addCirc = function(e) {
-	console.log("SVG");
-	if (this == e.target) {
-		svg.appendChild(makeCirc(e.offsetX, e.offsetY));
-	}
+    console.log("SVG");
+    if (this == e.target) {
+	svg.appendChild(makeCirc(e.offsetX, e.offsetY));
+    }
 };
 
 //clear screen
 var clear = function(e) {
-	while (svg.hasChildNodes()) {
-		svg.removeChild(svg.lastChild);
-	}
+    while (svg.hasChildNodes()) {
+	svg.removeChild(svg.lastChild);
+    }
 };
 
 //animate / 'move' button
 var anim = function(e){
-	window.cancelAnimationFrame( rid );
- 
-	var bounce = function() {
-		var circs = document.getElementsByTagName("circle")
-		for (var i=0; i<circs.length; i++) { //for all circles
-			var curx = circs[i].getAttribute("cx");
-			var cury = circs[i].getAttribute("cy");
-			var r = circs[i].getAttribute("r");
+    window.cancelAnimationFrame( rid );
+    
+    var bounce = function() {
+	var circs = document.getElementsByTagName("circle")
+	for (var i=0; i<circs.length; i++) { //for all circles
+	    var curx = parseInt(circs[i].getAttribute("cx"));
+	    var cury = parseInt(circs[i].getAttribute("cy"));
+	    var xvol = parseInt(circs[i].getAttribute("xvol"));
+	    var yvol = parseInt(circs[i].getAttribute("yvol"));
+	    var r = parseInt(circs[i].getAttribute("r"));
 
-			//adjust x/y accordingly based on boundaries
-			if ((curx > width-r) || (curx<0)) { 
-				curx--; 
-			} else {
-				curx++;
-			}
+	    //adjust x/y accordingly based on boundaries
+	    if (curx > width-r) { 
+		circs[i].setAttribute("xvol",-1);
+	    }else if (curx <= 0){
+		circs[i].setAttribute("xvol",1);
+	    }
 
-			if ((cury > height-r) || (cury < 0)) {
-				curx--;
-			} else {
-				cury++;
-			}
+	    if (cury > height-r) {
+		circs[i].setAttribute("yvol",-1);
+	    }else if (cury <= 0) {
+		circs[i].setAttribute("yvol",1);
+	    }
 
-			//change circle positions
-			circs[i].setAttribute("cx",curx);
-			circs[i].setAttribute("cy",cury);
-		}
-		//animate!
-		rid = window.requestAnimationFrame(bounce);
+	    console.log(xvol);
+	    console.log(yvol);
+	    console.log(r);
+	    console.log("\n");
+	    console.log("\n");
+	    curx += xvol;
+	    cury += yvol;
+	    //change circle positions
+	    circs[i].setAttribute("cx",curx);
+	    circs[i].setAttribute("cy",cury);
 	}
-	bounce();
+	//animate!
+	rid = window.requestAnimationFrame(bounce);
+    }
+    bounce();
 
 };
 
